0abfad728bd7129c702db2f0bb1dd6f3bb09c0bb
mehedi <mehedi25122003@gmail.com>
2025-02-06 22:21:47 +0600

    added some js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.getElementById('seat').addEventListener('click',)
+document.getElementById('seat').addEventListener('click',function(event){
+    this.classList.add('bg-green-400');
+})
+
+const displayMinusSeats = document.getElementById('show-seat').innerText;
+const displayMainSeats = parseFloat(displayMinusSeats);
