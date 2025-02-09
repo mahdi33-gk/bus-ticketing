226bf0976dcf3153fefaaf83ae962df781b98aca
mehedi <mehedi25122003@gmail.com>
2025-02-09 22:39:51 +0600

    finally btn cupon complete

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function allInOne(event) {
   document.getElementById("noSeatText").classList.add("hidden");
   const priceAddSection = document.getElementById("seatAdd");
+  const cuponApplyBtn = document.getElementById("cupon-appy-btn");
   let priceContainer = document.getElementById("price-container");
   priceContainer.classList.add("flex", "items-center", "flex-col");
 
@@ -26,34 +27,36 @@ function allInOne(event) {
   const availableSeat = oldAvailableSeat - 1;
   document.getElementById("main-seats").innerText = availableSeat;
 
-  const bookedSeat = oldBookedSeat + 1;
+  let bookedSeat = oldBookedSeat + 1;
   document.getElementById("booked-seat").innerText = bookedSeat;
 
   // price show section
   document.getElementById("total-price").innerText = 550 * bookedSeat;
   
-
+  if(bookedSeat>3){
+    cuponApplyBtn.removeAttribute('disabled');
+  }
  
 
  
 }
  // cupon Appy section
-let totalPrice = parseFloat(document.getElementById("total-price").innerText);
-document
-.getElementById("cupon-input")
-.addEventListener('keyup', function (event) {
-  let grandPrice = document.getElementById("after-cupon-price");
-  let cuponInput = document.getElementById('cupon-input').value;
+// let totalPrice = parseFloat(document.getElementById("total-price").innerText);
+// document
+// .getElementById("cupon-input")
+// .addEventListener('keyup', function (event) {
+//   let grandPrice = document.getElementById("after-cupon-price");
+//   let cuponInput = document.getElementById('cupon-input').value;
   
-  const cuponApplyBtn = document.getElementById("cupon-appy-btn");
-  if (cuponInput === "NEW15") {
-    cuponApplyBtn.removeAttribute("disabled");
-    let displayGrand = totalPrice - totalPrice * (15 / 100);
-    grandPrice.innerText = displayGrand;
-  } else if (cuponInput === "Couple15") {
-    cuponApplyBtn.classList.remove("disabled");
-    let displayGrand = totalPrice - totalPrice * (20 / 100);
-    grandPrice.innerText = displayGrand;
-  } else {
-  }
-});
+//   const cuponApplyBtn = document.getElementById("cupon-appy-btn");
+//   if (cuponInput === "NEW15") {
+//     cuponApplyBtn.removeAttribute("disabled");
+//     let displayGrand = totalPrice - totalPrice * (15 / 100);
+//     grandPrice.innerText = displayGrand;
+//   } else if (cuponInput === "Couple15") {
+//     cuponApplyBtn.re("disabled");
+//     let displayGrand = totalPrice - totalPrice * (20 / 100);
+//     grandPrice.innerText = displayGrand;
+//   } else {
+//   }
+// });
