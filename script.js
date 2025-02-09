e93264a8249559c35c4d193930d8fd9a6fa834d6
mehedi <mehedi25122003@gmail.com>
2025-02-09 22:14:40 +0600

    almost there

--- script.js
+++ script.js
@@ -1,31 +1,59 @@
-function allInOne(event){
+function allInOne(event) {
+  document.getElementById("noSeatText").classList.add("hidden");
+  const priceAddSection = document.getElementById("seatAdd");
+  let priceContainer = document.getElementById("price-container");
+  priceContainer.classList.add("flex", "items-center", "flex-col");
 
-    document.getElementById('noSeatText').classList.add('hidden');
-    const priceAddSection = document.getElementById('seatAdd');
-    let priceContainer = document.getElementById('price-container');
-    priceContainer.classList.add('flex', 'items-center', 'flex-col');
+  let li = document.createElement("li");
+  li.classList.add("flex", "justify-between", "w-full");
 
-    let li = document.createElement('li');
-    li.classList.add('flex', 'justify-between', 'w-full');
-
-    li.innerHTML = `
+  li.innerHTML = `
     <span> ${event.innerText}</span>
     <span>Economy</span>
     <span>550</span>
-    `
+    `;
 
-    
-    priceContainer.appendChild(li);
-    
-   
+  priceContainer.appendChild(li);
 
-    event.classList.add('bg-secondary', 'text-white');
-    const oldAvailableSeat = parseFloat(document.getElementById('main-seats').innerText);
-    const oldBookedSeat = parseFloat(document.getElementById('booked-seat').innerText);
+  event.classList.add("bg-secondary", "text-white");
+  const oldAvailableSeat = parseFloat(
+    document.getElementById("main-seats").innerText
+  );
+  const oldBookedSeat = parseFloat(
+    document.getElementById("booked-seat").innerText
+  );
 
-    const availableSeat = oldAvailableSeat - 1;
-    document.getElementById('main-seats').innerText = availableSeat;
+  const availableSeat = oldAvailableSeat - 1;
+  document.getElementById("main-seats").innerText = availableSeat;
 
-    const bookedSeat = oldBookedSeat + 1;
-    document.getElementById('booked-seat').innerText = bookedSeat;
+  const bookedSeat = oldBookedSeat + 1;
+  document.getElementById("booked-seat").innerText = bookedSeat;
+
+  // price show section
+  document.getElementById("total-price").innerText = 550 * bookedSeat;
+  
+
+ 
+
+ 
 }
+ // cupon Appy section
+let totalPrice = parseFloat(document.getElementById("total-price").innerText);
+document
+.getElementById("cupon-input")
+.addEventListener('keyup', function (event) {
+  let grandPrice = document.getElementById("after-cupon-price");
+  let cuponInput = document.getElementById('cupon-input').value;
+  
+  const cuponApplyBtn = document.getElementById("cupon-appy-btn");
+  if (cuponInput === "NEW15") {
+    cuponApplyBtn.removeAttribute("disabled");
+    let displayGrand = totalPrice - totalPrice * (15 / 100);
+    grandPrice.innerText = displayGrand;
+  } else if (cuponInput === "Couple15") {
+    cuponApplyBtn.classList.remove("disabled");
+    let displayGrand = totalPrice - totalPrice * (20 / 100);
+    grandPrice.innerText = displayGrand;
+  } else {
+  }
+});
