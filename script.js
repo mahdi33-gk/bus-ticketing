ed421075fa40890ba511accec42e749e855bb4ed
mehedi <mehedi25122003@gmail.com>
2025-02-09 20:46:59 +0600

    modal done

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function allInOne(event){
 
+    document.getElementById('noSeatText').classList.add('hidden');
+    const priceAddSection = document.getElementById('seatAdd');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <p>${event.innerText} </p>
+    <p>Economy</p>
+    <p>550</p>
+    `
+    priceAddSection.appendChild('div');
+
     event.classList.add('bg-secondary', 'text-white');
     const oldAvailableSeat = parseFloat(document.getElementById('main-seats').innerText);
     const oldBookedSeat = parseFloat(document.getElementById('booked-seat').innerText);
