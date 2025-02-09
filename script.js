2c8eabf36782411147a1654a6a5abc087e96f124
mehedi <mehedi25122003@gmail.com>
2025-02-09 20:27:35 +0600

    available seat/booked seat dynamic done

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function allInOne(event){
 
     event.classList.add('bg-secondary', 'text-white');
-    const availableSeat = document.getElementById('main-seats');
-    const bookedSeat = document.getElementById();
+    const oldAvailableSeat = parseFloat(document.getElementById('main-seats').innerText);
+    const oldBookedSeat = parseFloat(document.getElementById('booked-seat').innerText);
 
+    const availableSeat = oldAvailableSeat - 1;
+    document.getElementById('main-seats').innerText = availableSeat;
+
+    const bookedSeat = oldBookedSeat + 1;
+    document.getElementById('booked-seat').innerText = bookedSeat;
 }
